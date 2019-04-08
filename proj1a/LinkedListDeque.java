47a4ca3c5a453415acea7eee2e6b83d5b7a31b28
xin <2443468442@qq.com>
2019-04-08 10:51:52 +0800

    revised version1

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -19,24 +19,29 @@ public class LinkedListDeque<T> {
  */
     public LinkedListDeque(){
         sentinel = new TNode(null, null, null);
-        sentinel.next = sentinel;
-        sentinel.prev = sentinel;
+      sentinel.prev = sentinel;
+       sentinel.next = sentinel;
         size = 0;
     }
 
     public void addFirst(T item){
-        sentinel.next = new TNode(item, sentinel, sentinel.next);
+        TNode node = new TNode(item, sentinel, sentinel.next);
+        sentinel.next.prev = node;
+        sentinel.next = node;
         size += 1;
     }
 
     public void addLast(T item){
-        sentinel.prev = new TNode(item, sentinel.prev, sentinel);
+        TNode node = new TNode(item, sentinel.prev, sentinel);
+        sentinel.prev.next = node;
+        sentinel.prev = node;
         size += 1;
     }
 
     public boolean isEmpty(){
         if(size == 0){
-            return true;        }
+            return true;
+        }
         return false;
     }
 
@@ -60,6 +65,7 @@ public class LinkedListDeque<T> {
     }
 
     public T removeLast(){
+
         T res = (T) sentinel.prev.item;
         sentinel.prev.prev.next = sentinel;
         sentinel.prev = sentinel.prev.prev;
@@ -87,5 +93,20 @@ public class LinkedListDeque<T> {
         }
         return getRecursive(index - 1);
     }
+
+//    public static void main(String[] args){
+//        LinkedListDeque<Integer> l = new LinkedListDeque<>();
+//        l.addLast(0);
+//        l.addLast(1);
+//        l.addLast(2);
+//        l.isEmpty();
+//        l.isEmpty();
+//        l.addLast(5);
+//        l.addLast(6);
+//        l.isEmpty();
+//        l.addLast(8);
+//        Integer a = l.removeFirst();
+//        System.out.println(a);
+//    }
 
 }
